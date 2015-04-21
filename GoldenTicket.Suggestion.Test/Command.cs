fb298ff2adad005c9b8b56b57f9dedefbaffe62a
svirin <svirin@gmail.com>
2015-04-21 22:36:37 +0300

    suggest command

--- GoldenTicket.Suggestion.Test/Command.cs
+++ GoldenTicket.Suggestion.Test/Command.cs
@@ -4,6 +4,7 @@ using GoldenTicket.Command.Interfaces;
 using GoldenTicket.Data.Interfaces;
 using GoldenTicket.Model;
 using Parse;
+using GoldenTicket.Suggestion.Apriori;
 
 namespace GoldenTicket.Suggestion.Test
 {
@@ -11,7 +12,12 @@ namespace GoldenTicket.Suggestion.Test
     {
         public void ExecuteCommand(UserRecientBlock item)
         {
+            var performedCollection = PerformCollection(item);
+        }
 
+        private ItemsetCollection PerformCollection(UserRecientBlock item)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
