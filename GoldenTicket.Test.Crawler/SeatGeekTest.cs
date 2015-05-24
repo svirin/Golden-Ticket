3792cd744b14f645ed6212557b0a8da5a845be13
Ido Ganzer <iganzer@utest.com>
2015-05-25 00:23:51 +0300

    |Add crawler tests proj

---
+++ GoldenTicket.Test.Crawler/SeatGeekTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using GoldenTicket.Crawler.Test.Crawlers;
+
+namespace GoldenTicket.Test.Crawler
+{
+    [TestFixture]
+    class SeatGeekTest
+    {
+        [Test]
+        public void SeatGeekTesting()
+        {
+            SeatGeekCrawler crawler = new SeatGeekCrawler();
+            IEnumerable<Model.Concert> concerts = crawler.GetConcerts("Robbie Williams", new DateTime(2016, 1,1));
+            Assert.NotNull(concerts);
+        }
+    }
+}
